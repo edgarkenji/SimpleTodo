d7709259bbc977d5ae7b754da13a462dce95d9c0
Edgar Kenji Yamamoto <edgar.yamamoto@gmail.com>
2020-10-23 14:50:33 -0300

    Initial Commit

---
+++ SimpleTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleTodo
+//
+//  Created by Edgar Kenji Yamamoto on 23/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleTodo/SimpleTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoApp.swift
+//  SimpleTodo
+//
+//  Created by Edgar Kenji Yamamoto on 23/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
